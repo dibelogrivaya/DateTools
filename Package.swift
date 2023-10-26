50498aca187ae8de46472224c85dc3a3490d79b9
di <diana.belogrivaya@gmail.com>
2023-10-26 11:07:06 +0200

    Update gitignore.

--- Package.swift
+++ Package.swift
@@ -4,20 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "DateTools",
+    name: "DateToolsSwift",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "DateTools",
-            targets: ["DateTools"]),
+            name: "DateToolsSwift",
+            targets: ["DateToolsSwift"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "DateTools"),
-        .testTarget(
-            name: "DateToolsTests",
-            dependencies: ["DateTools"]),
+            name: "DateToolsSwift",
+            dependencies: [],
+            path: "DateToolsSwift/DateTools"
+        ),
     ]
 )
