6259fbf3a843d4f52a1a3f9fd65989672cb377b6
di <diana.belogrivaya@gmail.com>
2023-10-26 11:02:09 +0200

    Support SPM.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "DateToolsSwift",
+    name: "DateTools",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DateTools",
+            targets: ["DateTools"]),
+    ],
     targets: [
-        Target(name: "DateToolsSwift")
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DateTools"),
+        .testTarget(
+            name: "DateToolsTests",
+            dependencies: ["DateTools"]),
     ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
